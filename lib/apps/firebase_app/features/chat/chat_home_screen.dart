64afaf380db368dbb95fb716828f220dd1b29c42
박기순 <50977788+kspark-sqisoft@users.noreply.github.com>
2023-12-19 11:22:20 +0900

    PopScope

--- lib/apps/firebase_app/features/chat/chat_home_screen.dart
+++ lib/apps/firebase_app/features/chat/chat_home_screen.dart
@@ -30,107 +30,128 @@ class _ChatHomeScreenState extends State<ChatHomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: const Icon(CupertinoIcons.home),
-        title: _isSearchingUsers
-            ? TextField(
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Name, Email, ...',
-                ),
-                autofocus: true,
-                style: const TextStyle(fontSize: 16, letterSpacing: 0.5),
-                onChanged: (value) {
-                  //search logic
-                  _searchUsers.clear();
-                  for (var user in _users) {
-                    if (user.name!
-                            .toLowerCase()
-                            .contains(value.toLowerCase()) ||
-                        user.email!
-                            .toLowerCase()
-                            .contains(value.toLowerCase())) {
-                      _searchUsers.add(user);
-                    }
-                  }
-                  setState(() {});
-                },
-              )
-            : const Text('Chat'),
-        actions: [
-          IconButton(
-              onPressed: () {
-                setState(() {
-                  _isSearchingUsers = !_isSearchingUsers;
-                });
-              },
-              icon: Icon(_isSearchingUsers
-                  ? CupertinoIcons.clear_circled_solid
-                  : Icons.search)),
-          IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return ProfileScreen(
-                        currentUser: APIs.me,
-                      );
+    return GestureDetector(
+      //화면 다른곳 클릭시 키보드 닫기
+
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: PopScope(
+        //true => normal backbutton, false => do nothing
+        //검색중 뒤로 가기 누르면 앱 나가지 않게 처리
+        canPop: !_isSearchingUsers,
+        onPopInvoked: (bool didPop) async {
+          log('onPopInvoked didPop:$didPop');
+          setState(() {
+            _isSearchingUsers = !_isSearchingUsers;
+          });
+          if (didPop) {
+            return;
+          }
+        },
+        child: Scaffold(
+          appBar: AppBar(
+            leading: const Icon(CupertinoIcons.home),
+            title: _isSearchingUsers
+                ? TextField(
+                    decoration: const InputDecoration(
+                      border: InputBorder.none,
+                      hintText: 'Name, Email, ...',
+                    ),
+                    autofocus: true,
+                    style: const TextStyle(fontSize: 16, letterSpacing: 0.5),
+                    onChanged: (value) {
+                      //search logic
+                      _searchUsers.clear();
+                      for (var user in _users) {
+                        if (user.name!
+                                .toLowerCase()
+                                .contains(value.toLowerCase()) ||
+                            user.email!
+                                .toLowerCase()
+                                .contains(value.toLowerCase())) {
+                          _searchUsers.add(user);
+                        }
+                      }
+                      setState(() {});
                     },
-                  ),
-                );
-              },
-              icon: const Icon(Icons.more_vert)),
-        ],
-      ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 10.0),
-        child: FloatingActionButton(
-          onPressed: () {},
-          child: const Icon(Icons.add_comment_rounded),
+                  )
+                : const Text('Chat'),
+            actions: [
+              IconButton(
+                  onPressed: () {
+                    setState(() {
+                      _isSearchingUsers = !_isSearchingUsers;
+                    });
+                  },
+                  icon: Icon(_isSearchingUsers
+                      ? CupertinoIcons.clear_circled_solid
+                      : Icons.search)),
+              IconButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return ProfileScreen(
+                            currentUser: APIs.me,
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  icon: const Icon(Icons.more_vert)),
+            ],
+          ),
+          floatingActionButton: Padding(
+            padding: const EdgeInsets.only(bottom: 10.0),
+            child: FloatingActionButton(
+              onPressed: () {},
+              child: const Icon(Icons.add_comment_rounded),
+            ),
+          ),
+          body: StreamBuilder(
+              stream: APIs.getAllUsers(),
+              builder: (context, snapshot) {
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+
+                  case ConnectionState.active:
+                  case ConnectionState.done:
+                    final usersDocs = snapshot.data?.docs;
+                    _users = usersDocs
+                            ?.map(
+                                (userDoc) => ChatUser.fromJson(userDoc.data()))
+                            .toList() ??
+                        [];
+
+                    if (_users.isNotEmpty) {
+                      return ListView.builder(
+                        itemCount: _isSearchingUsers
+                            ? _searchUsers.length
+                            : _users.length,
+                        padding: EdgeInsets.only(top: mq.height * .01),
+                        physics: const BouncingScrollPhysics(),
+                        itemBuilder: (context, index) => ChatUserCard(
+                          chatUser: _isSearchingUsers
+                              ? _searchUsers[index]
+                              : _users[index],
+                        ),
+                      );
+                    } else {
+                      return const Center(
+                        child: Text(
+                          'No Users Collections Found!',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      );
+                    }
+                }
+              }),
         ),
       ),
-      body: StreamBuilder(
-          stream: APIs.getAllUsers(),
-          builder: (context, snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.waiting:
-              case ConnectionState.none:
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-
-              case ConnectionState.active:
-              case ConnectionState.done:
-                final usersDocs = snapshot.data?.docs;
-                _users = usersDocs
-                        ?.map((userDoc) => ChatUser.fromJson(userDoc.data()))
-                        .toList() ??
-                    [];
-
-                if (_users.isNotEmpty) {
-                  return ListView.builder(
-                    itemCount:
-                        _isSearchingUsers ? _searchUsers.length : _users.length,
-                    padding: EdgeInsets.only(top: mq.height * .01),
-                    physics: const BouncingScrollPhysics(),
-                    itemBuilder: (context, index) => ChatUserCard(
-                      chatUser: _isSearchingUsers
-                          ? _searchUsers[index]
-                          : _users[index],
-                    ),
-                  );
-                } else {
-                  return const Center(
-                    child: Text(
-                      'No Users Collections Found!',
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  );
-                }
-            }
-          }),
     );
   }
 }
