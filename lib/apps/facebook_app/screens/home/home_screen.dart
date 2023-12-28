3389a4abb3ce6afcc34c0a940e51a17f89a0733a
박기순 <50977788+kspark-sqisoft@users.noreply.github.com>
2023-12-28 17:01:24 +0900

    ok

--- lib/apps/facebook_app/screens/home/home_screen.dart
+++ lib/apps/facebook_app/screens/home/home_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+final users = ['', 'Metallica', '정수봉', '김대열', '성민창', 'iJustine'];
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -101,6 +104,7 @@ class HomeScreen extends StatelessWidget {
             thickness: 8,
           ),
         ),
+        //story
         SliverToBoxAdapter(
           child: SizedBox(
             height: 170,
@@ -160,21 +164,22 @@ class HomeScreen extends StatelessWidget {
                                       ],
                                     ),
                                     const Positioned(
-                                        top: 80,
-                                        left: 30,
+                                      top: 80,
+                                      left: 30,
+                                      child: CircleAvatar(
+                                        backgroundColor: Colors.white,
+                                        radius: 22,
                                         child: CircleAvatar(
-                                          backgroundColor: Colors.white,
-                                          radius: 22,
-                                          child: CircleAvatar(
-                                            radius: 20,
-                                            backgroundColor: Colors.blue,
-                                            child: FaIcon(
-                                              FontAwesomeIcons.plus,
-                                              color: Colors.white,
-                                              size: 20,
-                                            ),
+                                          radius: 20,
+                                          backgroundColor: Colors.blue,
+                                          child: FaIcon(
+                                            FontAwesomeIcons.plus,
+                                            color: Colors.white,
+                                            size: 20,
                                           ),
-                                        ))
+                                        ),
+                                      ),
+                                    ),
                                   ],
                                 ),
                               )
@@ -206,6 +211,30 @@ class HomeScreen extends StatelessWidget {
                                 width: 130,
                                 height: 200,
                               ),
+                              Positioned(
+                                top: 5,
+                                left: 5,
+                                child: CircleAvatar(
+                                  backgroundColor: Colors.white,
+                                  radius: 20,
+                                  child: CircleAvatar(
+                                    radius: 18,
+                                    backgroundColor: Colors.blue,
+                                    backgroundImage: AssetImage(
+                                        'assets/images/facebook/user$index.jpg'),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                bottom: 0,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Text(
+                                    users[index],
+                                    style: const TextStyle(color: Colors.white),
+                                  ),
+                                ),
+                              ),
                             ],
                           ),
                         ),
@@ -216,8 +245,250 @@ class HomeScreen extends StatelessWidget {
               },
             ),
           ),
+        ),
+        const SliverToBoxAdapter(
+          child: Divider(
+            thickness: 8,
+          ),
+        ),
+        //content1
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 18,
+                        backgroundColor: Colors.blue,
+                        backgroundImage:
+                            AssetImage('assets/images/facebook/user3.jpg'),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            '김대열',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 11),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            children: [
+                              const Text(
+                                '회원님을 위한 추천 · 36분 · ',
+                                maxLines: 2,
+                                style: TextStyle(fontSize: 11),
+                              ),
+                              FaIcon(
+                                FontAwesomeIcons.earthAmericas,
+                                size: 16,
+                                color: Colors.grey[500],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        '···',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const FaIcon(
+                            FontAwesomeIcons.xmark,
+                            color: Colors.grey,
+                          ))
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                '소원을 빌면 복이 온다는 ㄷㄷ',
+                maxLines: 2,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              StaggeredGrid.count(
+                crossAxisCount: 4,
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 4,
+                children: const [
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 2,
+                    mainAxisCellCount: 2,
+                    child: Tile(index: 0),
+                  ),
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 2,
+                    mainAxisCellCount: 1,
+                    child: Tile(index: 1),
+                  ),
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 1,
+                    mainAxisCellCount: 1,
+                    child: Tile(index: 2),
+                  ),
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 1,
+                    mainAxisCellCount: 1,
+                    child: Tile(index: 3),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+
+        //content1
+        SliverToBoxAdapter(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      const CircleAvatar(
+                        radius: 18,
+                        backgroundColor: Colors.blue,
+                        backgroundImage:
+                            AssetImage('assets/images/facebook/user3.jpg'),
+                      ),
+                      const SizedBox(
+                        width: 5,
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Text(
+                            '정수봉',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold, fontSize: 11),
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          Row(
+                            children: [
+                              const Text(
+                                '회원님을 위한 추천 · 36분 · ',
+                                maxLines: 2,
+                                style: TextStyle(fontSize: 11),
+                              ),
+                              FaIcon(
+                                FontAwesomeIcons.earthAmericas,
+                                size: 16,
+                                color: Colors.grey[500],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Text(
+                        '···',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                      IconButton(
+                          onPressed: () {},
+                          icon: const FaIcon(
+                            FontAwesomeIcons.xmark,
+                            color: Colors.grey,
+                          ))
+                    ],
+                  )
+                ],
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              const Text(
+                '이겨라(짝) 이겨라(짝)\n연기력 오지는 레전드 장면...',
+                maxLines: 2,
+              ),
+              const SizedBox(
+                height: 5,
+              ),
+              StaggeredGrid.count(
+                crossAxisCount: 4,
+                mainAxisSpacing: 4,
+                crossAxisSpacing: 4,
+                children: const [
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 1,
+                    mainAxisCellCount: 1,
+                    child: Tile2(index: 1),
+                  ),
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 1,
+                    mainAxisCellCount: 1,
+                    child: Tile2(index: 2),
+                  ),
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 2,
+                    mainAxisCellCount: 2,
+                    child: Tile2(index: 3),
+                  ),
+                  StaggeredGridTile.count(
+                    crossAxisCellCount: 2,
+                    mainAxisCellCount: 1,
+                    child: Tile2(index: 0),
+                  ),
+                ],
+              ),
+            ],
+          ),
         )
       ],
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile({super.key, required this.index});
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/images/facebook/story$index.jpg',
+      fit: BoxFit.cover,
+    );
+  }
+}
+
+class Tile2 extends StatelessWidget {
+  const Tile2({super.key, required this.index});
+  final int index;
+
+  @override
+  Widget build(BuildContext context) {
+    return Image.asset(
+      'assets/images/facebook/content$index.jpg',
+      fit: BoxFit.cover,
     );
   }
 }
