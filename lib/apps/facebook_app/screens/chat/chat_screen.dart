f8324cdc11cea1ffc2b1249798979e9a344488aa
박기순 <50977788+kspark-sqisoft@users.noreply.github.com>
2024-01-03 12:59:41 +0900

    slidable

--- lib/apps/facebook_app/screens/chat/chat_screen.dart
+++ lib/apps/facebook_app/screens/chat/chat_screen.dart
@@ -1,14 +1,46 @@
 import 'dart:developer';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:go_router/go_router.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 import '../home/home_screen.dart';
 
-class ChatScreen extends StatelessWidget {
+final List<String> chats = [
+  'Please try again, or scroll up to choose',
+  '나',
+  '안녕하세요 방가워요 그냥 친하게 지내고 싶어요',
+  '회원님과  박상삼님이 10년 전 Facebook 친구 에요',
+  '안녕하세요 방가워요 그냥 친하게 지내고 싶어요',
+  '안녕하세요 방가워요 그냥 친하게 지내고 싶어요',
+];
+final List<String> chatDates = [
+  ' · 11월 4일',
+  ' · 2020.04.27',
+  ' · 2020.02.17',
+  ' · 2019.11.17',
+  ' · 2019.05.12',
+  ' · 2019.08.02',
+];
+
+class ChatScreen extends StatefulWidget {
   const ChatScreen({super.key});
+
+  @override
+  State<ChatScreen> createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
+  int _currentTabIndex = 0;
+
+  @override
+  void initState() {
+    _tabController =
+        TabController(vsync: this, length: 2, animationDuration: Duration.zero);
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -122,9 +154,179 @@ class ChatScreen extends StatelessWidget {
                   ),
                 ),
               ),
+            ),
+            SliverFillRemaining(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  TabBar(
+                    controller: _tabController,
+                    indicatorColor: Colors.transparent,
+                    dividerColor: Colors.transparent,
+                    tabs: [
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: _currentTabIndex == 0
+                                ? Colors.blue.withOpacity(0.1)
+                                : Colors.transparent,
+                            elevation: 0,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _currentTabIndex = 0;
+                            });
+
+                            _tabController.animateTo(_currentTabIndex,
+                                duration: Duration.zero);
+                          },
+                          child: const Text('받은 메시지함')),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: _currentTabIndex == 1
+                                ? Colors.blue.withOpacity(0.1)
+                                : Colors.transparent,
+                            elevation: 0,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              _currentTabIndex = 1;
+                            });
+                            _tabController.animateTo(_currentTabIndex,
+                                duration: Duration.zero);
+                          },
+                          child: const Text('커뮤니티')),
+                    ],
+                  ),
+                  Expanded(
+                    child: TabBarView(
+                      controller: _tabController,
+                      physics: const NeverScrollableScrollPhysics(),
+                      children: const [
+                        MailBox(),
+                        Community(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             )
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MailBox extends StatelessWidget {
+  const MailBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SlidableAutoCloseBehavior(
+      child: ListView.builder(
+        physics: const NeverScrollableScrollPhysics(),
+        padding: EdgeInsets.zero,
+        itemCount: 20,
+        itemBuilder: (context, index) => Slidable(
+          key: ValueKey(index),
+          closeOnScroll: true,
+          endActionPane: ActionPane(
+            extentRatio: .75,
+            dragDismissible: false,
+            motion: const BehindMotion(),
+            dismissible: DismissiblePane(onDismissed: () {}),
+            children: [
+              CustomSlidableAction(
+                onPressed: (BuildContext context) {},
+                backgroundColor: Colors.grey[200]!,
+                foregroundColor: Colors.black,
+                child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FaIcon(FontAwesomeIcons.inbox),
+                      Text(
+                        '더 보기',
+                        style: TextStyle(fontSize: 11),
+                      )
+                    ]),
+              ),
+              CustomSlidableAction(
+                onPressed: (BuildContext context) {},
+                backgroundColor: Colors.grey[200]!,
+                foregroundColor: Colors.black,
+                child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FaIcon(FontAwesomeIcons.bellSlash),
+                      Text(
+                        '알림 해제',
+                        style: TextStyle(fontSize: 11),
+                      )
+                    ]),
+              ),
+              CustomSlidableAction(
+                onPressed: (BuildContext context) {},
+                backgroundColor: Colors.pinkAccent,
+                foregroundColor: Colors.black,
+                child: const Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      FaIcon(FontAwesomeIcons.boxArchive),
+                      Text(
+                        '보관',
+                        style: TextStyle(fontSize: 11),
+                      )
+                    ]),
+              ),
+            ],
+          ),
+          child: ListTile(
+            leading: CircleAvatar(
+              backgroundColor: Colors.white,
+              radius: 20,
+              child: CircleAvatar(
+                radius: 18,
+                backgroundColor: Colors.blue,
+                backgroundImage:
+                    AssetImage('assets/images/facebook/user${index % 6}.jpg'),
+              ),
+            ),
+            title: Text(users[index % 6]),
+            subtitle: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(
+                      chats[index % 6],
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                      style: const TextStyle(fontSize: 12),
+                    ),
+                  ),
+                  Text(
+                    chatDates[index % 6],
+                    maxLines: 1,
+                    style: const TextStyle(fontSize: 12),
+                  ),
+                ]),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Community extends StatelessWidget {
+  const Community({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      physics: const NeverScrollableScrollPhysics(),
+      padding: EdgeInsets.zero,
+      itemCount: 20,
+      itemBuilder: (context, index) => const ListTile(
+        title: Text('Md. Samim Sarkar'),
       ),
     );
   }
