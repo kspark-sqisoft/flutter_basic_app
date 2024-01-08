934ade668382daee37c23867988f3cb562aeb82a
박기순 <50977788+kspark-sqisoft@users.noreply.github.com>
2024-01-08 14:43:36 +0900

    friendscreen

--- lib/apps/facebook_app/screens/friend/friend_screen.dart
+++ lib/apps/facebook_app/screens/friend/friend_screen.dart
@@ -1,12 +1,213 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class FriendScreen extends StatelessWidget {
   const FriendScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('친구')),
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          pinned: true,
+          scrolledUnderElevation: 0,
+          title: const Text('친구'),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Row(
+                children: [
+                  CircleAvatar(
+                    maxRadius: 20,
+                    backgroundColor: Colors.grey[300]!,
+                    child: const FaIcon(
+                      FontAwesomeIcons.magnifyingGlass,
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+        SliverPadding(
+          padding: const EdgeInsets.all(8),
+          sliver: SliverToBoxAdapter(
+            child: Column(children: [
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[100],
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30), // <-- Radius
+                        ),
+                      ),
+                      child: const Text('추천'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.grey[100],
+                        foregroundColor: Colors.black,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30), // <-- Radius
+                        ),
+                      ),
+                      child: const Text('내 친구'),
+                    ),
+                  ),
+                ],
+              ),
+              const Divider(),
+            ]),
+          ),
+        ),
+        const SliverPadding(
+          padding: EdgeInsets.all(8),
+          sliver: SliverToBoxAdapter(
+            child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '친구 요청',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '모두 보기',
+                    style: TextStyle(color: Colors.lightBlue),
+                  ),
+                ]),
+          ),
+        ),
+        SliverList.builder(
+          itemCount: 7,
+          itemBuilder: (context, index) => Container(
+            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Image.asset(
+                      'assets/images/facebook/friend$index.jpg',
+                      fit: BoxFit.cover,
+                      width: 100,
+                      height: 100,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text('JY Nadia Kim'),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Stack(
+                            alignment: Alignment.centerLeft,
+                            children: [
+                              const SizedBox(
+                                width: 44,
+                                height: 30,
+                              ),
+                              Positioned(
+                                left: 16,
+                                child: CircleAvatar(
+                                  radius: 10,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: Image.asset(
+                                      'assets/images/facebook/friend1.jpg',
+                                      fit: BoxFit.cover,
+                                      width: 20,
+                                      height: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Positioned(
+                                child: CircleAvatar(
+                                  radius: 10,
+                                  child: ClipRRect(
+                                    borderRadius: BorderRadius.circular(10),
+                                    child: Image.asset(
+                                      'assets/images/facebook/friend2.jpg',
+                                      fit: BoxFit.cover,
+                                      width: 20,
+                                      height: 20,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          const Text(
+                            '함께 아는 친구 2명',
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.lightBlue,
+                                foregroundColor: Colors.white,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.circular(8), // <-- Radius
+                                ),
+                              ),
+                              child: const Text('확인'),
+                            ),
+                          ),
+                          const SizedBox(
+                            width: 10,
+                          ),
+                          Expanded(
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Colors.grey[200],
+                                foregroundColor: Colors.black,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius:
+                                      BorderRadius.circular(8), // <-- Radius
+                                ),
+                              ),
+                              child: const Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    FaIcon(FontAwesomeIcons.pen, size: 16),
+                                    Text('삭제')
+                                  ]),
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+        )
+      ],
     );
   }
 }
