744f737160369a8bb981711e42af047523c0e177
박기순 <50977788+kspark-sqisoft@users.noreply.github.com>
2024-01-09 11:22:18 +0900

    constrainedBox upgrade

--- lib/apps/basic_app/features/constrainedbox/constrained_box_screen.dart
+++ lib/apps/basic_app/features/constrainedbox/constrained_box_screen.dart
@@ -27,7 +27,7 @@ class ConstrainedBoxScreen extends StatelessWidget {
                   ),
                 ),
                 const SizedBox(
-                  height: 10,
+                  height: 50,
                 ),
                 //자식을 제한 한다.하위 항목에 추가 제약을 가하는 위젯, SizedBox.expand 위젯을 사용하여 동일한 동작을 얻을 수 있습니다.
                 const Text('ConstrainedBox'),
@@ -42,6 +42,69 @@ class ConstrainedBoxScreen extends StatelessWidget {
                     child: const Text(
                       'Delicious Candy',
                       style: TextStyle(fontSize: 90),
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Text('Container'),
+                Card(
+                  elevation: 1,
+                  child: Image.asset(
+                    'assets/images/facebook/profile.jpg', //original 719x720
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Text('ConstrainedBox'),
+                ConstrainedBox(
+                  constraints: const BoxConstraints(
+                    maxWidth: 200,
+                    maxHeight: 200,
+                    minWidth: 100,
+                    minHeight: 100,
+                  ),
+                  child: Card(
+                    elevation: 1,
+                    child: Image.asset(
+                      'assets/images/facebook/profile.jpg', //original 719x720
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Text('ConstrainedBox.tightFor'),
+                ConstrainedBox(
+                  constraints: const BoxConstraints.tightFor(),
+                  child: Card(
+                    elevation: 1,
+                    child: Image.asset(
+                      'assets/images/facebook/profile.jpg', //original 719x720
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                const Text('ConstrainedBox.expand'),
+                Container(
+                  width: 400,
+                  height: 400,
+                  color: Colors.lightGreen,
+                  child: ConstrainedBox(
+                    constraints: const BoxConstraints.expand(),
+                    child: Card(
+                      elevation: 1,
+                      child: Image.asset(
+                        'assets/images/facebook/profile.jpg', //original 719x720
+                        fit: BoxFit.cover,
+                      ),
                     ),
                   ),
                 ),
@@ -76,6 +139,9 @@ class ConstrainedBoxScreen extends StatelessWidget {
                     child: Image.network(
                         'https://picsum.photos/200/300?random=20'),
                   ),
+                ),
+                const SizedBox(
+                  height: 50,
                 ),
                 const Text('FractionallySizedBox'),
                 Container(
